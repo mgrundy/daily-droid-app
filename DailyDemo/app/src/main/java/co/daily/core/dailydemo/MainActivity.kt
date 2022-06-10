35b2213bdcebc9d471a93ac672402933cacd29b0
Michael Grundy <michael.grundy@animoto.com>
2022-06-09 21:11:17 -0400

    Modifications for Entri demo

--- DailyDemo/app/src/main/java/co/daily/core/dailydemo/MainActivity.kt
+++ DailyDemo/app/src/main/java/co/daily/core/dailydemo/MainActivity.kt
@@ -33,21 +33,19 @@ class MainActivity : AppCompatActivity() {
         }
 
     private lateinit var addurl: EditText
+    private lateinit var callCount: TextView
     private lateinit var callClient: CallClient
-    private var localContainer: FrameLayout? = null
-    private var remoteContainer: FrameLayout? = null
-    private var localVideoView: VideoView? = null
-    private var remoteVideoView: VideoView? = null
+
+    // ok, I'm going for it
+    private var remoteContainers = arrayOfNulls<FrameLayout?>(3)
+    private var remoteVideoViews = arrayOfNulls<VideoView?>(3)
+    private var visibleParticipants = arrayOfNulls<Participant?>(3)
+    private var participantCount = 0
 
     private lateinit var joinButton: Button
     private lateinit var leaveButton: Button
-    private lateinit var micInputButton: ImageButton
-    private lateinit var camInputButton: ImageButton
-    private lateinit var micPublishButton: ImageButton
-    private lateinit var camPublishButton: ImageButton
 
-    private lateinit var localCameraMaskView: TextView
-    private lateinit var remoteCameraMaskView: TextView
+//    private lateinit var remoteCameraMaskView: TextView
 
     private var callClientListener = object : CallClientListener {
 
@@ -63,7 +61,10 @@ class MainActivity : AppCompatActivity() {
                     joinButton.visibility = View.GONE
                     leaveButton.visibility = View.VISIBLE
                     leaveButton.isEnabled = true
-                    updateRemoteCameraMaskViewMessage()
+                    addurl.visibility = View.GONE
+                    // callCount.visibility = View.VISIBLE
+                    // This is sus on high participant calls
+                    //updateRemoteCameraMaskViewMessage()
                 }
                 CallState.leaving -> {}
                 CallState.left -> {
@@ -75,32 +76,29 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
-        override fun onInputsUpdated(inputSettings: InputSettings) {
-            Log.d(TAG, "On Inputs updated ${InputSettings}")
-            refreshInputPublishButtonsState()
+         override fun onParticipantJoined(participant: Participant) {
+             // updateRemoteCameraMaskViewMessage()
+             if (!participant.info.isLocal && participant.media?.camera?.track != null) {
+                 Log.d(TAG, "onParticipantJoined yesVid local ${participant.info.isLocal}")
+                 updateParticipantVideoView(participant)
+             } else {
+                 Log.d(TAG, "onParticipantJoined noVid, local: ${participant.info.isLocal}")
 
-            val cameraEnabled = callClient.inputs().camera.isEnabled
-            localCameraMaskView.visibility = if (cameraEnabled) View.GONE else View.VISIBLE
-            localVideoView?.visibility = if (cameraEnabled) View.VISIBLE else View.GONE
-        }
-
-        override fun onPublishingUpdated(publishingSettings: PublishingSettings) {
-            refreshInputPublishButtonsState()
-        }
-
-        override fun onParticipantJoined(participant: Participant) {
-            Log.d(TAG, "onParticipantJoined local ${participant.info.isLocal}")
-            updateParticipantVideoView(participant)
+             }
         }
 
         override fun onParticipantUpdated(participant: Participant) {
             Log.d(TAG, "onParticipantUpdated local ${participant.info.isLocal}")
+            // check if participant is in presenting participant list
             updateParticipantVideoView(participant)
         }
 
         override fun onParticipantLeft(participant: Participant) {
             Log.d(TAG, "onParticipantLeft")
+            // Check if it's one of our video on participants
             renderNextParticipantTrack()
+            // updateRemoteCameraMaskViewMessage()
+
         }
     }
 
@@ -110,33 +108,32 @@ class MainActivity : AppCompatActivity() {
         joinButton.isEnabled = true
         leaveButton.isEnabled = false
         leaveButton.visibility = View.GONE
-        clearLocalVideoView()
+        addurl.visibility = View.VISIBLE
+        callCount.visibility = View.GONE
         clearRemoteVideoView()
-        remoteCameraMaskView.text = resources.getText(R.string.join_meeting_instructions)
+        // remoteCameraMaskView.text = resources.getText(R.string.join_meeting_instructions)
     }
 
     private fun renderNextParticipantTrack(){
+        // Proably don't need this function because
         Log.d(TAG, "renderNextParticipantTrack")
         // Render the next particpant in the list if any
-        val nextParticipant =
-            callClient.participants().all.values.firstOrNull{ !it.info.isLocal && it.media?.camera?.track != null }
-        updateRemoteVideoTrack(nextParticipant?.media?.camera?.track)
+//        val nextParticipant =
+//            callClient.participants().all.values.firstOrNull{ !it.info.isLocal && it.media?.camera?.track != null }
+//        updateRemoteVideoTrack(nextParticipant?.media?.camera?.track)
     }
 
     private fun clearRemoteVideoView() {
-        remoteVideoView?.release()
-        remoteContainer?.removeView(remoteVideoView)
-        remoteVideoView = null
-        remoteCameraMaskView.visibility = View.VISIBLE
+        for (i in remoteVideoViews.indices) {
+            remoteVideoViews[i]?.release()
+            remoteContainers[i]?.removeView(remoteVideoViews[i])
+            remoteVideoViews[i] = null
+            visibleParticipants[i] = null
+            // remoteCameraMaskView.visibility = View.VISIBLE
+        }
     }
 
-    private fun clearLocalVideoView() {
-        localVideoView?.release()
-        localContainer?.removeView(localVideoView)
-        localVideoView = null
-        localCameraMaskView.visibility = View.VISIBLE
-    }
-
+    // This is sus on high participant calls. Need to investigate
     private fun updateRemoteCameraMaskViewMessage() {
         val message =
             when (val amountOfParticipants = callClient.participants().all.size) {
@@ -144,38 +141,59 @@ class MainActivity : AppCompatActivity() {
                 2 -> callClient.participants().all.filter { !it.value.info.isLocal }.entries.first().value.info.userName
                 else -> resources.getString(R.string.amount_of_participants_at_meeting, amountOfParticipants-1)
             }
-        remoteCameraMaskView.text = message
+        callCount.text = message
+    }
+
+    private fun checkDisplaySlots(participant: Participant):Int {
+        // ugly check for which slot is open and if the participant is in already
+        var openSlot = -1
+        var key = -1
+        for (i in visibleParticipants.indices) {
+            if (visibleParticipants[i] == null && openSlot < 0) {
+                openSlot = i
+            } else if (visibleParticipants[i]?.id == participant.id) {
+                key = i
+                break
+            }
+        }
+
+        if ((key < 0 && openSlot < 0)) {
+            key = -1
+        } else if (key < 0) {
+            key = openSlot
+            visibleParticipants[key] = participant
+        }
+        return key
     }
 
     private fun updateParticipantVideoView(participant: Participant){
         if (participant.info.isLocal) {
-            if (localVideoView == null) {
-                localVideoView = VideoView(this@MainActivity)
-                localContainer?.addView(localVideoView)
-                localVideoView?.bringVideoToFront()
-            }
-            localVideoView?.track = participant.media?.camera?.track
-        }else {
-            if(participant.media?.camera?.track != null){
-                updateRemoteVideoTrack(participant.media?.camera?.track)
-            }else {
-                renderNextParticipantTrack()
+            Log.d(TAG, "Participant: ${participant.id} is local")
+        } else {
+            Log.d(TAG, "Participant ${participant.id} joined the call!")
+
+            val key = checkDisplaySlots(participant)
+            if (key >= 0) {
+                updateRemoteVideoTrack(key, participant.media?.camera?.track)
+                android.util.Log.d(TAG, "track updated ${participant.media?.camera?.track?.nativeTrack}")
+            } else {
+                Log.d(TAG, "No free slot for Participant ${participant.id}")
             }
         }
-        Log.d(TAG, "track updated ${participant.media?.camera?.track?.nativeTrack}")
     }
 
-    private fun updateRemoteVideoTrack(track: MediaStreamTrack? = null) {
+    private fun updateRemoteVideoTrack(key: Int, track: MediaStreamTrack? = null) {
         Log.d(TAG, "updateRemoteVideoTrack ${track}")
-        if (remoteVideoView == null) {
-            remoteVideoView = VideoView(this@MainActivity)
-            remoteContainer?.addView(remoteVideoView)
+        if (remoteVideoViews[key] == null) {
+            remoteVideoViews[key] = VideoView(this@MainActivity)
+            remoteContainers[key]?.addView(remoteVideoViews[key])
         }
-        remoteVideoView?.track = track
+        remoteVideoViews[key]?.track = track
+        // hopefully it isn't null at this point
         val containsTrack = track != null
-        remoteCameraMaskView.visibility = if(containsTrack) View.GONE else View.VISIBLE
-        remoteVideoView?.visibility = if(containsTrack) View.VISIBLE else View.GONE
-        updateRemoteCameraMaskViewMessage()
+        // remoteCameraMaskView.visibility = if(containsTrack) View.GONE else View.VISIBLE
+        remoteVideoViews[key]?.visibility = if(containsTrack) View.VISIBLE else View.GONE
+
     }
 
     override fun onCreate(savedInstance: Bundle?) {
@@ -185,17 +203,15 @@ class MainActivity : AppCompatActivity() {
 
         joinButton = findViewById(R.id.call_button)
         leaveButton = findViewById(R.id.hangup_button)
-        micInputButton = findViewById(R.id.input_mic_button)
-        camInputButton = findViewById(R.id.input_camera_button)
-        micPublishButton = findViewById(R.id.publish_mic_button)
-        camPublishButton = findViewById(R.id.publish_camera_button)
 
-        localContainer = findViewById(R.id.local_video_view_container)
-        remoteContainer = findViewById(R.id.remote_video_view_container)
+        remoteContainers[0] = findViewById(R.id.remote_video_view_container_1)
+        remoteContainers[1] = findViewById(R.id.remote_video_view_container_2)
+        remoteContainers[2] = findViewById(R.id.remote_video_view_container_3)
 
-        localCameraMaskView = findViewById(R.id.local_camera_mask_view)
-        remoteCameraMaskView = findViewById(R.id.remote_camera_mask_view)
         addurl = findViewById(R.id.aurl)
+        // addurl.setText("https://michael.staging.daily.co/playground")
+        callCount = findViewById(R.id.participantCountView)
+        callCount.visibility = View.GONE
 
         checkPermissions()
     }
@@ -205,97 +221,16 @@ class MainActivity : AppCompatActivity() {
         initEventListeners()
     }
 
-    private fun toggleCamInput(enabled:Boolean) {
-        lifecycleScope.launch() {
-            callClient.updateInputs(
-                inputSettings = InputSettingsUpdate(
-                    camera = if (enabled) Enable() else Disable()
-                )
-            )
-        }
-    }
-
-    private fun toggleMicInput(enabled:Boolean) {
-        Log.d(TAG, "toggleMicInput ${enabled}")
-        lifecycleScope.launch() {
-            callClient.updateInputs(
-                inputSettings = InputSettingsUpdate(
-                    microphone = if (enabled) Enable() else Disable()
-                )
-            )
-        }
-    }
-
-    private fun toggleMicPublish(enabled:Boolean) {
-        Log.d(TAG, "toggleMicPublish ${enabled}")
-        lifecycleScope.launch() {
-            callClient.updatePublishing(
-                publishSettings = PublishingSettingsUpdate(
-                    microphone = MicrophonePublishingSettingsUpdate(
-                        isPublishing = if (enabled) Enable() else Disable()
-                    )
-                )
-            )
-        }
-    }
-
-    private fun toggleCamPublish(enabled:Boolean) {
-        Log.d(TAG, "toggleMicPublish ${enabled}")
-        lifecycleScope.launch() {
-            callClient.updatePublishing(
-                publishSettings = PublishingSettingsUpdate(
-                    camera = CameraPublishingSettingsUpdate(
-                        isPublishing = if (enabled) Enable() else Disable()
-                    )
-                )
-            )
-        }
-    }
-
-    private fun refreshInputPublishButtonsState() {
-        micInputButton.backgroundTintList = if (callClient.inputs().microphone.isEnabled)
-            resources.getColorStateList(R.color.colorEnabled, null) else
-            resources.getColorStateList(R.color.colorDisabled, null)
-        camInputButton.backgroundTintList = if (callClient.inputs().camera.isEnabled)
-            resources.getColorStateList(R.color.colorEnabled, null) else
-            resources.getColorStateList(R.color.colorDisabled, null)
-        micPublishButton.backgroundTintList = if (callClient.publishing().microphone.isPublishing)
-            resources.getColorStateList(R.color.colorEnabled, null) else
-            resources.getColorStateList(R.color.colorDisabled, null)
-        camPublishButton.backgroundTintList = if (callClient.publishing().camera.isPublishing)
-            resources.getColorStateList(R.color.colorEnabled, null) else
-            resources.getColorStateList(R.color.colorDisabled, null)
-    }
-
     private fun createClientSettingsIntent():ClientSettingsUpdate {
-        val publishingSettingsIntent = PublishingSettingsUpdate(
-            camera = CameraPublishingSettingsUpdate(
-                sendSettings = VideoSendSettingsUpdate(
-                    encodings = VideoEncodingsSettingsUpdate(
-                        settings = mapOf(
-                            VideoSendSettingsMaxQualityUpdate.low to
-                                    VideoEncodingSettingsUpdate(
-                                        maxBitrate = BitRate(80000),
-                                        maxFramerate = FrameRate(10),
-                                        scaleResolutionDownBy = Scale(4F)
-                                    ),
-                            VideoSendSettingsMaxQualityUpdate.medium to
-                                    VideoEncodingSettingsUpdate(
-                                        maxBitrate = BitRate(680000),
-                                        maxFramerate = FrameRate(30),
-                                        scaleResolutionDownBy = Scale(1F)
-                                    )
-                        )
-                    )
-                ),
-                enableInputOnPublish = Enable()
-            ),
-            microphone = MicrophonePublishingSettingsUpdate(
-                enableInputOnPublish = Enable()
-            )
-        )
         return ClientSettingsUpdate(
-            publishingSettings = publishingSettingsIntent
+                inputSettings = InputSettingsUpdate(
+                    camera = CameraInputSettingsUpdate(
+                        isEnabled = Disable(),
+                    ),
+                    microphone = MicrophoneInputSettingsUpdate(
+                        isEnabled = Disable(),
+                    )
+                )
         )
     }
 
@@ -351,30 +286,12 @@ class MainActivity : AppCompatActivity() {
             it.isEnabled = false
             callClient.leave()
         }
-
-        micInputButton.setOnClickListener {
-            toggleMicInput(!callClient.inputs().microphone.isEnabled)
-        }
-
-        camInputButton.setOnClickListener {
-            toggleCamInput(!callClient.inputs().camera.isEnabled)
-        }
-
-        micPublishButton.setOnClickListener {
-            toggleMicPublish(!callClient.publishing().microphone.isPublishing)
-        }
-
-        camPublishButton.setOnClickListener {
-            toggleCamPublish(!callClient.publishing().camera.isPublishing)
-        }
     }
 
     private fun initCallClient() {
         callClient = CallClient(applicationContext).apply {
             addListener(callClientListener)
         }
-        toggleMicInput(true)
-        toggleCamInput(true)
     }
 
     override fun onDestroy() {
